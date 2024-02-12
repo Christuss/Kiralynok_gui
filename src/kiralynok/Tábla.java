ac117d9d7e6e73467bf2892f0d298e281c9908df
christuss <szendefi.krisztofer@gmail.com>
2024-02-12 13:32:09 +0100

    elso feladat

---
+++ src/kiralynok/Tábla.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kiralynok;
+
+/**
+ *
+ * @author User
+ */
+public class Tábla {
+    String[][] T;
+    String[] UresCella;
+
+    
+    
+    void UresOszlopokSzama() {
+        
+    }
+    
+    void UresSorokSzama() {
+        
+    }
+    
+    void Elhelyez() {
+        
+    }
+    
+    void FajlbaIr() {
+        
+    }
+    
+    void Megjelenit() {
+        
+    }
+    
+    void Tabla() {
+        
+    }
+    
+    void UresOszlop() {
+        
+    }
+    
+    void UresSor() {
+        
+    }
+}
